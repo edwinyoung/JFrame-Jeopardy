39f40edfb755d45846d9dd8e2af0ff4de458df6c
Edwin Young <edwin.w.young@gmail.com>
2012-07-30 11:32:27 -0700

    Creating new JFrame-based Jeopardy

---
+++ src/LiteraryJeopardy.java
@@ -0,0 +1,21 @@
+/**
+ * Literary Jeopardy application
+ *
+ * @Edwin Young
+ * @version 1.00 2008/5/31
+ */
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.Timer;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LiteraryJeopardy {
+	
+	public static final long serialVersionUID = 1;
+	
+}
